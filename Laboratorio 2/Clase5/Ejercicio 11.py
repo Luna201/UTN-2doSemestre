1dfa58828e773f32ea359ed55d3f7d9c95aa0134
Andres Luna <andresluna201@hotmail.com>
2024-02-29 17:54:11 -0300

    ej 11 completo

--- Laboratorio 2/Clase5/Ejercicio 11.py
+++ Laboratorio 2/Clase5/Ejercicio 11.py
@@ -8,15 +8,35 @@ el programa tendra el siguiente menu de opciones:
 4. Salir"""
 
 agenda = {}
-while True
+while True:
     print("\n .:MENÚ:.")
     print("1. Nuevo contacto")
     print("2. Borrar contacto")
     print("3. Ver contactos existentes")
     print("4. Salir")
     opcion = int(input("Digite una opción de menú: "))
-    if opcion == 1
+    if opcion == 1:
         nombre = input("Digite el nombre del contacto: ")
         telefono = input("Digite el número de teefono: ")
         if nombre not in agenda:
-            agenda[nombre] = 
+            agenda[nombre] = telefono
+            print("¡Contacto agregado exitosamente!")
+        else:
+            print("Este nombre de contacto ya existe")
+    elif opcion == 2:
+        nombre = input("Cual es el nombre del contacto: ")
+        if nombre in agenda:
+            del (agenda[nombre])
+            print("Se ha eliminado el contacto requerido")
+        else:
+            print("El contacto no existe en la agenda")
+    elif opcion == 3:
+        print("Agenda de contactos")
+        for clave, valor in agenda.items(): #muestra clave y valor
+            print(f"Nombre: {clave}, Teléfono: {valor}")
+    elif opcion == 4:
+        print("Gracias por utilizar su agenda de contacto")
+        break
+    else:
+        print("Se equivoco de opcion de menú")
+        print( )
