1f4781097d6c978485fcd5e7f197cd0850828f91
Andres Luna <andresluna201@hotmail.com>
2024-03-01 01:49:00 -0300

    agruegue archivos de clase 6

--- Laboratorio 2/Clase5/FuncionesDePython/FuncionesDePython.py
+++ Laboratorio 2/Clase5/FuncionesDePython/FuncionesDePython.py
@@ -6,3 +6,38 @@ def mi_funcion():   #snake_case o camelCase
 
 mi_funcion()    #Llamamos la función
 mi_funcion()
+
+
+# CLASE 6
+#DEsempaquetado de listas o list Unpacking
+def show(name, lastName):
+    print(name+" "+lastName)
+person = ["Ariel", "Betancud"]
+show(person[0], person[1])  #Pasamos uno por uno  los datos de la lista a la funcion
+show(*person)   # con el asterisco muestra todo
+person2 = ("Ovaldo", "Giordanini")  #desampaquetamos atraves de una TUPLA
+show(*person2)
+person3 = {"lastName": "Lucero", "name": "Natalia"}
+show(**person3)
+
+numbers =[1, 2, 3, 4, 5] # se ejecuta aunque este vacio  el else a menos que este el break
+for n in numbers:
+    print(n)
+    if n == 3:
+        break   #Esta es la unica manera para que no se ejecute el else
+else:
+    print("Esto se termina")
+
+
+# List comprehesion, lista de compresion. NO modifica la lista, SOLO extrae
+names = ["Paolo","Rodrigo", "Lupe", "Pepe"]
+alongP = [p for p in names if p[0]  == "P" ] #Genera una nueva lista
+print(alongP)
+
+bottleC = [{"name": "Quilmes", "country": "Arg"},
+           {"name": "Corona", "country": "Mex"},
+           {"name": "Stella Artois", "country": "Belgium"},
+            ]
+Arg = [b for b in bottleC if b["country"] == "Arg"]
+print(Arg)
+print(bottleC)
