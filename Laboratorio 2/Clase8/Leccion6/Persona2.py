d56f18f17b74affa21902aafdda89393d3e3888e
Andres Luna <andresluna201@hotmail.com>
2024-04-16 19:40:11 -0300

    agregue video de 3 a 6 de clase 10 en python

--- Laboratorio 2/Clase8/Leccion6/Persona2.py
+++ Laboratorio 2/Clase8/Leccion6/Persona2.py
@@ -23,7 +23,7 @@ class Persona2:
         return self._apellido
 
     @apellido.setter
-    def apellido(self, apellido):  # Método setter
+    def apellido(self, apellido):  # Método setter (necesita incluir un parametro, apellido)
         print("Estamos usando el método set")
         self._apellido = apellido
 
@@ -38,7 +38,8 @@ class Persona2:
         self._edad = edad
 
 
-persona1 = Persona2("Ariel", "Betancud", 41)
+if __name__ == "__main__":
+    persona1 = Persona2("Ariel", "Betancud", 41)
 print(persona1.nombre)  # o   print(persona1.nombre())    Llamamos al método getter
 
 persona1 = Persona2("Ariel", "Betancud", 41)
@@ -47,5 +48,38 @@ print(persona1.apellido)
 persona1 = Persona2("Ariel", "Betancud", 41)
 print(persona1.edad)
 
-persona1.nombre= "Juan Pedro"
+persona1.nombre = "Juan Pedro"
 print(persona1.nombre)
+print(persona1.nombre)  # otra vez con el metodo getter
+print(persona1.mostrar_detalles())  # Llamamos el método mostrar_detalles
+# Atributo read-only seria la edad porque no tiene el método setter
+print(persona1.edad)
+
+# EJERCICIO:
+# Crear 3 objetos más, utilizandolos métodos getter and setter para modificar, y mostrar los cambios con
+# el método mostrar_detalles
+persona2 = Persona2("Flor", "Romero", 23)
+persona2.nombre = "Florencia"
+persona2.apellido = "Romery"
+persona2.edad = 22
+print(persona2.mostrar_detalles())
+
+persona3 = Persona2("Caro", "Felisa", 21)
+persona3.nombre = "Carolina"
+persona3.apellido = "Romery"
+persona3.edad = 31
+print(persona3.nombre)
+print(persona3.apellido)
+print(persona3.edad)
+print(persona3.mostrar_detalles())
+
+persona4 = Persona2("Naty", "Lucer", 35)
+persona4.nombre = "Natalia"
+persona4.apellido = "Lucero"
+persona4.edad = 33
+print(persona4.nombre)
+print(persona4.apellido)
+print(persona4.edad)
+print(persona4.mostrar_detalles())
+
+print(__name__)
---
+++ Laboratorio 2/Clase8/Leccion6/PruebaPersona2.py
@@ -0,0 +1,6 @@
+from Persona2 import Persona2
+
+if __name__ == "__main__":
+    persona5 = Persona2("Lionel", "Messi", 35)
+    persona5.mostrar_detalles()
+    print(__name__)     #dice en que parte del codigo estamos
