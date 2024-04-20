b89ba3075095849b06c265dceec22244bdb7aa70
Andres Luna <andresluna201@hotmail.com>
2024-04-20 00:01:37 -0300

    añadi ejercicio a clase 11 de python, video 4

--- Laboratorio 2/Clase11/Persona.py
+++ Laboratorio 2/Clase11/Persona.py
@@ -1,3 +1,4 @@
+"""
 class Persona():
     def __init__(self, nombre, edad):  # Esta clase hereda de la clase Objet
         self.nombre = nombre
@@ -9,8 +10,65 @@ class Empleado(Persona):  # Esta clase es hija de la clase persona
         super().__init__(nombre, edad)
         self.sueldo = sueldo
 
+empleado1 = Empleado("Ariel", 40, 75000)
+print(empleado1.nombre)
+print(empleado1.edad)
+print(empleado1.sueldo)
+
+
+Tarea: Encapsular los atributos y agregar los métodos getters and setters
+Crear otro objeto, pasar los datos para nombre, edad y sueldo, mostrar los datos, luego modificar
+y mostrar nuevamente"""
+
+
+class Persona():
+    def __init__(self, nombre, edad):  # Esta clase hereda de la clase Objet
+        self._nombre = nombre
+        self._edad = edad
+
+    @property
+    def nombre(self):
+        return self._nombre
+
+    @nombre.setter
+    def nombre(self, nombre):
+        self._nombre = nombre
+
+    @property
+    def edad(self):
+        return self._edad
+
+    @edad.setter
+    def edad(self, edad):
+        self._edad = edad
+
+
+class Empleado(Persona):  # Esta clase es hija de la clase persona
+    def __init__(self, nombre, edad, sueldo):
+        super().__init__(nombre, edad)
+        self.sueldo = sueldo
+
+    @property
+    def sueldo(self):
+        return self._sueldo
+
+    @sueldo.setter
+    def sueldo(self, sueldo):
+        self._sueldo = sueldo
+
 
 empleado1 = Empleado("Ariel", 40, 75000)
 print(empleado1.nombre)
 print(empleado1.edad)
 print(empleado1.sueldo)
+
+empleado2 = Empleado("Liliana", 38, 70000)
+print(empleado2.nombre)
+print(empleado2.edad)
+print(empleado2.sueldo)
+empleado2.nombre = "Natalia"
+empleado2.edad = 35
+empleado2.sueldo = 75000
+print(empleado2.nombre)
+print(empleado2.edad)
+print(empleado2.sueldo)
