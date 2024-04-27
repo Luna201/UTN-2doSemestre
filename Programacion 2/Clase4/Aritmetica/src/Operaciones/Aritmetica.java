6340193c675f2a6099b6df3b579d861fb4429e90
Andres Luna <andresluna201@hotmail.com>
2024-04-27 12:14:58 -0300

    corregi los videos de metodos de la clase 5 en java

--- Programacion 2/Clase4/Aritmetica/src/Operaciones/Aritmetica.java
+++ Programacion 2/Clase4/Aritmetica/src/Operaciones/Aritmetica.java
@@ -4,8 +4,25 @@ public class Aritmetica {
     int a;
     int b;
     
-    //El constructor es un Metodo especial. Sobrecarga de constructor = 1 o mas constructores, agregando  u omitiendo valores
-    public Aritmetica(){    //constructor 1, vacio, por defecto
-        System.out.println("Se esta ejecutando este constructor número uno");
+    //Método
+    public void sumarNumeros(){ //void no retorna
+        int resultado = a+ b;
+        System.out.println("resultado= "+ resultado);
     }
+    
+    public int sumarConRetorno(){
+        //int resultado = a + b;
+        return a + b;
+        
+    }    
+    public int sumarConArgumentos(int arg1, int arg2){
+        a = arg1;
+        b = arg2;
+        //return a + b;
+        return sumarConRetorno();   //se puede llamar a oro método solo si comparte la misma clase
+    }
+    //El constructor es un Metodo especial. Sobrecarga de constructor = 1 o mas constructores, agregando  u omitiendo valores
+    //public Aritmetica(){    //constructor 1, vacio, por defecto
+    //    System.out.println("Se esta ejecutando este constructor número uno");
+    //}
 }
--- Programacion 2/Clase4/Aritmetica/src/Operaciones/PruebaAritmetica.java
+++ Programacion 2/Clase4/Aritmetica/src/Operaciones/PruebaAritmetica.java
@@ -4,7 +4,14 @@ public class PruebaAritmetica {
         Aritmetica aritmetica1 = new Aritmetica();
         aritmetica1.a = 3;
         aritmetica1.b = 7;
+        aritmetica1.sumarNumeros();
         
+    int resultado = aritmetica1.sumarConRetorno();
+        System.out.println("resultado= "+ resultado);
         
+        resultado = aritmetica1.sumarConArgumentos(12, 26);
+        System.out.println("resultado usando argumentos= "+ resultado);
     }
+        
 }
+
